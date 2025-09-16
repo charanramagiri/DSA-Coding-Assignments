6407f6582c57cab46952726185fb5e08cbe521db
charan <charanramagiri2004@gmail.com>
2025-09-16 23:19:22 +0530

    updated queue using stacks

--- Queues/QueueUsingStacks.java
+++ Queues/QueueUsingStacks.java
@@ -37,3 +37,53 @@ class MyQueue {
         return st2.isEmpty() && st1.isEmpty();
     }
 }
+
+
+
+//with main code
+/*
+ 
+ import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution.
+        Scanner sc = new Scanner(System.in);
+        
+        int q = sc.nextInt();
+        
+        Stack<Integer> st1 = new Stack<>();
+        Stack<Integer> st2 = new Stack<>();
+        
+        for(int i=0;i<q;i++){
+            int type = sc.nextInt();
+            
+            if(type == 1){
+                int val = sc.nextInt();
+                st1.push(val);
+            }
+            
+            else if(type==2){
+                if(st2.isEmpty()){
+                    while(!st1.isEmpty()){
+                        st2.push(st1.pop());
+                    }
+                }
+                st2.pop();
+            }
+            
+            else if(type ==3){
+                if(st2.isEmpty()){
+                    while(!st1.isEmpty()){
+                        st2.push(st1.pop());
+                    }
+                }
+                System.out.println(st2.peek());
+            }
+        }   
+    }
+}
+
+*/
